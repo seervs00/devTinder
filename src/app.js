9adcbb3708e73177ccb8228eef88eeff3c73c26f
seervs00 <mahendras00b@gmail.com>
2024-12-21 17:42:20 +0530

    feed api find all the data from database

--- src/app.js
+++ src/app.js
@@ -4,6 +4,35 @@ const app = express();
 // const {userauth,adminauth} = require("./middlewares/auth")
 const User = require("./models/user")
 app.use(express.json());
+
+app.get("/user", async(req,res) => {
+    const users = await User.findOne({emailId:req.body.emailId});
+    try {
+        
+        res.send(users);
+    }
+    catch (err) {
+        console.error("Error saving user:", err);
+        res.status(500).send("Error saving user");
+    }
+});
+
+app.get("/feed", async(req,res) => {
+    const users = await User.find();
+    try {
+        if(users.length == 0){
+            res.send("user not found");
+        }
+        else{
+        res.send(users);
+    }
+}
+    catch (err) {
+        console.error("Error saving user:", err);
+        res.status(500).send("Error saving user");
+    }
+});
+
 app.post("/signup",async (req, res) => {
     //Creating a new instance of the user model
     const user = new User(req.body);
