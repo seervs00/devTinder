4746f15f6ed37915b326d3814be50a13bec3dc21
seervs00 <mahendras00b@gmail.com>
2025-01-23 22:40:39 +0530

    cors plugin

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -43,7 +43,7 @@ authRouter.post("/login",async(req,res) =>{
         const token = await user.getJWT();
         res.cookie("token",token,{expires: new Date(Date.now() + 8 * 3600000)});
         // expire  your cokies in 8 hour
-        res.json({message: "login successfully!!"});
+        res.json(user);
     }
     else{
         throw new Error("Invalid  credentials");  
